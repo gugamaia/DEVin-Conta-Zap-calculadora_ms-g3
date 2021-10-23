d99dac774cd0e263b18c4a531979587bd0c642fe
Gustavo Maia <gustavojv_maia@outlook.com>
2021-10-23 10:22:50 -0300

    v01
    Creating the app with React

---
+++ calculadora-ms-g3/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import React,{useState} from 'react';
+
+export default function App() {
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default App;
