fcced7316e80eceb8e6443170ae6b7e6d50a2c39
Gustavo Maia <gustavojv_maia@outlook.com>
2021-10-23 18:36:30 -0300

    v1.1

--- calculadora-ms-g3/src/App.js
+++ calculadora-ms-g3/src/App.js
@@ -3,6 +3,43 @@ import './App.css';
 import React,{useState} from 'react';
 
 export default function App() {
+  const [valorTela,setValorTela]=useState('')
+  const [resultado,setResultado]=useState(0)
+  const [acumulador,setAcumulador]=useState(0)
+  const [operacao,setOperacao]=useState(false)
+
+  const Tela=(valor,resultadoFinal)=> { 
+    return(
+      <div style={}>
+        <span style={}>{valor}</span>
+        <span sytle={}>{resultadoFinal}</span>
+      </div>
+    )
+  }
+
+  const cssTela={
+    display: 'flex',
+    paddingLeft: 20,
+    paddingRight: 20,
+    justifycontent:'center',
+    alignItems: 'flex-start',
+    backgroundColor: '#111',
+    flexDirection: 'column',
+    width: 260
+  }
+
+  const cssTelaOperacao={
+    fontSize:25,
+    color: '#fff',
+    height:20
+  }
+
+  const cssTelaResiltado={
+    fontSize:50,
+    color: '#fff',
+    height:20
+  }
+
   return (
     <>
 
